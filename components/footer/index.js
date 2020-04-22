a8e37385b5163da35f405294400c8848275ff60a
Joao Portela <joao.portela@gmail.com>
2020-04-22 15:55:07 +0100

    updated footer: zeit->vercel

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <p className={styles.container}>
       Made with ❤️ and <a href="https://nextjs.org/">Next.js</a>.
-      Hosted on <a href="https://zeit.co/">ZEIT</a>.
+      Hosted on <a href="https://vercel.com/">Vercel</a>.
       Source at <a href="https://github.com/jportela/www-joaoportela/">GitHub</a>.
     </p>
   )
